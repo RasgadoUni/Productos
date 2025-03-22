bf2b504c81106297d38145d63ee50f2758e23674
RasgadoUni <145424911+RasgadoUni@users.noreply.github.com>
2025-03-22 11:19:39 -0600

    Update productos.routes.ts

--- src/routes/productos.routes.ts
+++ src/routes/productos.routes.ts
@@ -1,8 +1,53 @@
-import { Router } from "express";
-import {getAll} from "../controllers/productos.controller.ts";
+import express from 'express';
 
-const router = Router();
+type Request = express.Request;
+type Response = express.Response;
+import connection from '../routes/db.ts';
 
-router.get("/all", getAll)
+export const getAll = (req: Request, res: Response) => {
+    connection.query('SELECT * FROM Productos', (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json(results);
+    });
+};
 
-export default router;
+export const update = (req: Request, res: Response) => {
+    const {ID} = req.params;
+    const { Nombre, Descripcion, Precio, Categoria} = req.body;
+   
+        const query = 'UPDATE Productos SET Nombre = ?, Descripcion = ?, Precio = ?, Categoria = ? WHERE ID = ?';
+        connection.query(query, [Nombre, Descripcion, Precio, Categoria, ID], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).json({ message: 'Se actualizo bien' });
+    });
+};
+
+export const insert = (req: Request, res: Response) => {
+    const {ID, Nombre, Descripcion, Precio, Categoria} = req.body;
+   
+        const query = 'INSERT INTO Productos (ID, Nombre, Descripcion, Precio, Categoria) VALUES (?, ?, ?, ?, ?)';
+        connection.query(query, [ID, Nombre, Descripcion, Precio, Categoria], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).json({ message: 'insercion correcta' });
+    });
+};
+
+export const elimina = (req: Request, res: Response) => {
+    const { ID } = req.params;
+    
+   
+        const query = 'DELETE FROM Productos WHERE ID = ?';
+        connection.query(query, [ID], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).json({ message: 'Se elimino bien' });
+    });
+};
