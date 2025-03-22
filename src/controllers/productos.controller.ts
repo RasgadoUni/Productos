4c4d5783dfc894c023d9fb0da04a39aa179399fb
RasgadoUni <145424911+RasgadoUni@users.noreply.github.com>
2025-03-22 11:18:35 -0600

    Update productos.controller.ts

--- src/controllers/productos.controller.ts
+++ src/controllers/productos.controller.ts
@@ -14,3 +14,40 @@ export const getAll = (req: Request, res: Response) => {
     });
 };
 
+export const update = (req: Request, res: Response) => {
+    const {ID} = req.params;
+    const { Nombre, Descripcion, Precio, Categoria} = req.body;
+   
+        const query = 'UPDATE Productos SET Nombre = ?, Descripcion = ?, Precio = ?, Categoria = ? WHERE ID = ?';
+        connection.query(query, [Nombre, Descripcion, Precio, Categoria, ID], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).json({ message: 'Se actualizo bien' });
+    });
+};
+
+export const insert = (req: Request, res: Response) => {
+    const {ID, Nombre, Descripcion, Precio, Categoria} = req.body;
+   
+        const query = 'INSERT INTO Productos (ID, Nombre, Descripcion, Precio, Categoria) VALUES (?, ?, ?, ?, ?)';
+        connection.query(query, [ID, Nombre, Descripcion, Precio, Categoria], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).json({ message: 'insercion correcta' });
+    });
+};
+
+export const elimina = (req: Request, res: Response) => {
+    const { ID } = req.params;
+    
+   
+        const query = 'DELETE FROM Productos WHERE ID = ?';
+        connection.query(query, [ID], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).json({ message: 'Se elimino bien' });
+    });
+};
